3f526402cfe048e48fcd4d8e72303e7750a924f8
Hypeeer <vittorjoao18@hotmail.com>
2024-10-14 20:24:40 -0300

    refactor-comentarios

--- funcoes/funcaos.js
+++ funcoes/funcaos.js
@@ -1,10 +1,9 @@
 import questionsAnswers from "../datas/datas.js"; //faz o import do arquivo datas.js
 import readlineSync from "readline-sync";
 
-let userResponse;
 //Função para gera aleatoriamente perguntas
 const randomQuestions = (arr) => {
-  const newArr = arr.sort(() => Math.random() - 0.5); //callBack
+  const newArr = arr.sort(() => Math.random() - 0.5); //(callBack) => Embaralha o array
   return newArr.map((item, index) => ({
     ...item, //spread operator ("espalha ou copia os elementos para outros array ou objetos")
     id: index + 1, // propriedade do novo objeto
@@ -12,7 +11,7 @@ const randomQuestions = (arr) => {
 };
 //Função para seleciona apenas 10 peguntas aleatorias
 const selectTen = () => {
-  return randomQuestions(questionsAnswers).slice(0, 10);
+  return randomQuestions(questionsAnswers).slice(0, 10); // separa 10 pergunta
 };
 //Função para exiber 10 perguntas formatadas e contabilizar os pontos
 const SystemOfVerificationAndCounter = () => {
@@ -20,7 +19,7 @@ const SystemOfVerificationAndCounter = () => {
   let counter = 0;
   tenQuestions.forEach((item) => {
     console.log(`\n${item.id} - ${item.question}`);
-    userResponse = readlineSync.question(`Digite a resposta: `);
+    let userResponse = readlineSync.question(`Digite a resposta: `);
     if (userResponse === item.answers) {
       console.log("Resposta Correta!\n");
       counter++;
@@ -48,4 +47,4 @@ const SystemOfMenssage = (name) => {
   console.log(`${msg}`);
 };
 
-export default SystemOfMenssage;
+export default SystemOfMenssage; // Exporta a saida da função para a raiz do projeto
--- main-quiz.js
+++ main-quiz.js
@@ -1,12 +1,12 @@
 import readlineSync from "readline-sync";
-import SystemOfMenssage from "./funcoes/funcaos.js";
+import SystemOfMenssage from "./funcoes/funcaos.js"; //Importa a saida da função
 
 //Função principal
 const mainMenu = () => {
   console.log(`----- Quiz JavaScript -----`);
   console.log(`Seja Bem-vindo jogador(a)!`);
   let name = readlineSync.question(`Digite seu nome: `);
-  SystemOfMenssage(name);
+  SystemOfMenssage(name); // Consome o "name" para a sainda na função
 };
 
 mainMenu();
